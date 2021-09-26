7f929fbbdce49814d642999659b1ff7184535417
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2021-09-26 09:21:06 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,17 +1,7 @@
-#importing modules
 import pandas as pd
+import csv
 import plotly.express as px
-
-#read csv data using pd module
 df = pd.read_csv("data.csv")
-
-#selecting a particular student
-student_df = df.loc[df["student_id"]=="TRL_zny"]
-
-#finding the mean of student of attempts in each level
-mean = student_df.groupby("level")["attempt"].mean()
-
-#creating a graph
-graph = px.scatter(mean, size=["attempt"], color=["attempt"])
-
-graph.show()
+mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
+fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt")
+fig.show()
